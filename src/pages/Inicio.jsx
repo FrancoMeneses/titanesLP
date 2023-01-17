db764af8cf5a02e8531bd4fa563f39847c035ec7
Franco Meneses <meneses773@gmail.com>
2023-01-16 19:56:13 -0600

    add changes

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -295,10 +295,11 @@ export const Inicio = () => {
           <div className="flex flex-col gap-y-6">
             <p className="text-3xl font-semibold text-center">Síguenos en nuestras redes sociales</p>
             <div className="flex justify-center gap-x-8">
-              <a className="transition ease-in duration-300 text-black hover:-translate-y-1 hover:scale-110 hover:text-rose-900" href="https://www.instagram.com/titanespueblaallstars/" target="_blank" rel="noopener noreferrer">
+              {/* <a className="transition ease-in duration-300 text-black hover:-translate-y-1 hover:scale-110 hover:text-rose-900" href="https://www.instagram.com/titanespueblaallstars/" target="_blank" rel="noopener noreferrer"> */}
+              <a className="flex justify-center items-center transition ease-in duration-300 w-14 h-14 hover:bg-gradient-to-b text-black hover:rounded-full hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 hover:text-white hover:-translate-y-1 hover:scale-110" href="https://www.instagram.com/titanespueblaallstars/" target="_blank" rel="noopener noreferrer">
                 <BsInstagram className="w-10 h-10"></BsInstagram>
               </a>
-              <a className="transition ease-in duration-300 text-black hover:-translate-y-1 hover:scale-110 hover:text-blue-700" href="https://www.facebook.com/TitanesPueblaallstars/" target="_blank" rel="noopener noreferrer">
+              <a className="flex justify-center items-center w-14 h-14 transition ease-in duration-300 text-black hover:-translate-y-1 hover:scale-110 hover:text-blue-700" href="https://www.facebook.com/TitanesPueblaallstars/" target="_blank" rel="noopener noreferrer">
                 <BsFacebook className="w-10 h-10"></BsFacebook>
               </a>
             </div>
