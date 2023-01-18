5b4781ef76631a8d7adcd52d89b18245553cd987
Franco Meneses <meneses773@gmail.com>
2023-01-17 18:00:57 -0600

    add google font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./index.html",
   "./src/**/*.{js,ts,jsx,tsx}", "./node_modules/tw-elements/dist/js/**/*.js"],
   theme: {
+    fontFamily: {
+      display: ["Montserrat", "sans-serif"]
+    },
     extend: {},
   },
   plugins: [require("tw-elements/dist/plugin")],
