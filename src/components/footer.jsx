e607e107fcd2d317f3c79c2852c6ce0daaf587e9
Franco Meneses <meneses773@gmail.com>
2023-03-27 08:41:54 -0600

    padding changed

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ import { BsInstagram, BsFacebook } from "react-icons/bs"
 
 export const Footer = () => {
   return (
-    <div className="flex flex-col gap-y-2 bg-[#1e1e1e]">
+    <div className="flex flex-col gap-y-2 bg-[#1e1e1e] p-3">
       <p className="text-lg font-semibold text-center text-white">Síguenos en nuestras redes sociales</p>
       <div className="flex justify-center gap-x-8">
         <a className="flex justify-center items-center transition ease-in duration-300 w-14 h-14 hover:bg-gradient-to-b text-white hover:rounded-full hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 hover:text-white hover:-translate-y-1 hover:scale-110" href="https://www.instagram.com/titanespueblaallstars/" target="_blank" rel="noopener noreferrer">
