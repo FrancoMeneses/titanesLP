1935a6859d8b88b51a0be2743129f13ba6e1b17d
Franco Meneses <meneses773@gmail.com>
2023-06-11 20:50:46 -0600

    context added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Footer, Inicio, Sucursal1, Sucursal2, Cheerleading, Parkour, Gimnasia, Aviso, Whatsapp } from './imports'
+import { Footer, Inicio, Sucursal1, Sucursal2, Cheerleading, Parkour, Gimnasia, Aviso, Whatsapp, WaForm, CursoVerano2023 } from './imports'
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
 
 import { Navbar } from './components/navbar'
@@ -6,10 +6,11 @@ import { Navbar } from './components/navbar'
 const HeaderLayout = () => {
   return(
     <div className="font-display">
-    <Navbar/>
-      <Outlet/>
-      <Whatsapp/>
-    <Footer/>
+        <Navbar/>
+          <WaForm></WaForm>
+          <Outlet/>
+          <Whatsapp/>
+        <Footer/>
     </div>
   )
 }
@@ -45,6 +46,10 @@ const router = createBrowserRouter([
       {
         path: "/aviso-privacidad",
         element: <Aviso />
+      },
+      {
+        path: "/curso-de-verano-2023",
+        element: <CursoVerano2023 />
       }
     ]
   }
